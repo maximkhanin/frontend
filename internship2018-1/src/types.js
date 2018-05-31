4ec591fe1d9d9600097dd5d2c3f467a77f4c00ec
120-W10-1803-VS\1 <maksimkhanin.kpi@gmail.com>
2018-05-31 14:49:03 +0300

    lesson 2

---
+++ internship2018-1/src/types.js
@@ -0,0 +1,14 @@
+'use strict'
+document.write("Helllo interns!")
+var myVariable = 45
+
+function func(){
+    var a=1,
+        b=2;
+    return a;
+}
+
+var c = func()
+console.log(c)
+console.log(myVariable)
+console.log(window);
