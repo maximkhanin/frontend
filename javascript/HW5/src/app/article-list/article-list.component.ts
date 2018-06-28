511f74945f1d134c5c9aa2a83ba2156c93199675
MaximKhanin <maksimkhanin.kpi@gmail.com>
2018-06-28 13:59:24 +0300

    HW5

---
+++ javascript/HW5/src/app/article-list/article-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { ArticleModel } from './article-details/article.model';
+import { ArticleListService } from './article-list.service';
+
+@Component({
+	selector: 'article-list',
+	templateUrl: './article-list.component.html'
+})
+export class ArticleListComponent {
+	articles: ArticleModel[];
+
+	constructor(private service: ArticleListService) {
+		this.articles = this.service.getArticles();
+	}
+}
---
+++ javascript/HW5/src/app/article-list/article-list.service.ts
@@ -0,0 +1,27 @@
+import { ArticleModel } from './article-details/article.model';
+
+export class ArticleListService {
+    articles: ArticleModel[] = [
+        {
+            id: 1,
+            title: 'Як знімали рекламу Apple в Україні',
+            shortDescription: 'Режисер кліпу Rolling in the Deep, скейтер з Іспанії та оператор на роликах з Південної Африки'
+        }, {
+            id: 2,
+            title: 'Ipsum excepteur nisi est culpa exercitation cupidatat.',
+            shortDescription: `Minim aute et cupidatat aliqua pariatur laboris incididunt minim laboris cillum ex anim sit.
+                Non tempor incididunt ut cillum aliquip dolor. Consectetur ipsum pariatur voluptate exercitation cillum elit labore.`
+        }, {
+            id: 3,
+            title: 'Cillum do exercitation aliqua laboris aliqua aute cupidatat eiusmod mollit eiusmod Lorem aute mollit mollit.',
+            shortDescription: `Irure laborum minim est laboris officia esse dolor esse enim sint voluptate tempor exercitation.
+                Sint veniam ad laborum occaecat consectetur ullamco voluptate aute sit deserunt cupidatat.
+                Aute exercitation elit pariatur ea velit ex commodo duis ea nostrud non.
+                Cillum consectetur elit in irure irure aliquip id deserunt aliquip.`
+        }
+    ];
+
+    getArticles(): ArticleModel[] {
+        return this.articles;
+    }
+}
